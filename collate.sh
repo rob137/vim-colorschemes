34048f6a3594a2a5c2086d6bd4b89b91c61d6f98
Rob Kirby <rob@universlabs.co.uk>
2020-06-30 12:32:01 +0100

    First pass at nice colorscheme script

--- collate.sh
+++ collate.sh
@@ -1,28 +1,41 @@
-bin bash stuff
-eou pipefail whatever
+#!/bin/sh
+set -euxo pipefail
 
-mkdir ./colorschemes
+TMPDIR="/tmp/colorschemes"
 
-" cp * from two directories to directory of this script:
-$DIRS = [
-	~/.vim/plugged/awesome-vim-colorschemes/colors/
-	~/.vim/plugged/vim-colorschemes/colors/
-]
-cp $dir/* in $DIRS to ./colorschemes/
+# Create dir if not already there
+mkdir -p $TMPDIR
 
-" cp my list of nice colorschemes:
-cp ~/dotfiles/nice-vim-colorschemes.txt .
+# cp my list of nice colorschemes:
+cp ~/dotfiles/nice-vim-colors.txt .
 
-" Run down the list of files in ./colorschemes, asking 'is it in the list of
-" nice colorschemes?'
-$COLORSCHEMES = ./colorschemes/*
-for $colorscheme in $COLORSCHEMES:
-	IF (1+ lines in nice-vim-colorschemes.txt is a substring of your name') 
-		rm $colorscheme
-	ENDIF
-ENDFOR
+# cp * from two directories to directory of this script:
+declare -a dirs=(
+  "/Users/univers/.vim/plugged/awesome-vim-colorschemes/colors/"
+ "/Users/univers/.vim/plugged/vim-colorschemes/colors/"
+)
+for dir in "${dirs[@]}"
+do
+ cp $dir* $TMPDIR
+done
 
-" Copy contents of ./colorschemes to somewhere vim looks for its colorschemes
-cp ./colorschemes/* ~/.vim/colors
+# Make array of nice colors
+nicecolors=""
+IFS=$'\n'       # make newlines the only separator
+for i in $(cat < /Users/univers/dotfiles/nice-vim-colors.txt); do
+  nicecolors+="$i.vim "
+done
 
+# Run down the list of files in ./colorschemes, asking 'is it in the list of
+# nice colorschemes?', deleting the ones that aren't
+colorpaths=( /tmp/colorschemes/* )
+for colorpath in "${colorpaths[@]}"; do
+	colorfile=$(basename $colorpath)
+	if ! [[ " ${nicecolors[@]} " =~ $colorfile ]]; then
+		rm "$colorpath"
+	fi
+done
+
+# Copy contents of ./colorschemes to somewhere vim looks for its colorschemes
+cp $TMPDIR/* /Users/univers/.vim/colors
 
